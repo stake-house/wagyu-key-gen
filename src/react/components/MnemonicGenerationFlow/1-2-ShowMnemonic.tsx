2bd4811f2b9e06561c4b0d05750847af1ad9da4f
Nicholas Baroni <nbaroni@tuta.io>
2021-08-15 21:58:17 +0900

    UX changes to mnemonic generation step

--- src/react/components/MnemonicGenerationFlow/1-2-ShowMnemonic.tsx
+++ src/react/components/MnemonicGenerationFlow/1-2-ShowMnemonic.tsx
@@ -1,18 +1,41 @@
-import { Tooltip, IconButton, Grid, TextField, Typography } from '@material-ui/core';
+import { Tooltip, IconButton, Grid, Typography } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 import { FileCopy } from '@material-ui/icons';
 import { clipboard } from 'electron';
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, Fragment, useState } from 'react';
+import styled from 'styled-components';
 
 type ShowMnemonicProps = {
   showCopyWarning: boolean,
   mnemonic: string,
 }
 
+const MnemonicDisplay = styled(Typography)`
+  font-size: 1.5rem;
+  color: white;
+  border: 1px solid gray;
+  padding: 1rem;
+`
+
 const ShowMnemonic: FC<ShowMnemonicProps> = (props): ReactElement => {
+  const [copyTooltipOpen, setCopyTooltipOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
+  
+  const handleCopyTooltipClose = () => {
+    setCopyTooltipOpen(false);
+    setTimeout(() => setCopied(false), 200);
+  };
+
+  const handleCopyTooltipOpen = () => {
+    setCopyTooltipOpen(true);
+  };
 
   const copyMnemonic = () => {
     clipboard.writeText(props.mnemonic);
-  }
+    setCopied(true);
+  };
+
+  const copyText = copied ? 'Copied' : 'Copy';
 
   return (
     <Grid container direction="column" spacing={3}>
@@ -22,18 +45,12 @@ const ShowMnemonic: FC<ShowMnemonicProps> = (props): ReactElement => {
       <Grid item container xs={12}>
         <Grid item xs={1} />
         <Grid item xs={10}>
-          <TextField
-            id="mnemonic-display"
-            value={props.mnemonic}
-            multiline
-            fullWidth
-            rows={4}
-            variant="outlined"
-            color="primary"
-            disabled />
+          <MnemonicDisplay id="mnemonic-display">
+            {props.mnemonic}
+          </MnemonicDisplay>
         </Grid>
         <Grid item xs={1} style={{alignSelf: "center"}}>
-          <Tooltip title="Copy">
+          <Tooltip title={copyText} open={copyTooltipOpen} onClose={handleCopyTooltipClose} onOpen={handleCopyTooltipOpen}>
             <IconButton aria-label="copy" color="primary" onClick={copyMnemonic} autoFocus>
               <FileCopy />
             </IconButton>
@@ -41,10 +58,10 @@ const ShowMnemonic: FC<ShowMnemonicProps> = (props): ReactElement => {
         </Grid>
       </Grid>
       { props.showCopyWarning &&
-        <Grid item xs={12}>
-          <Typography color="error">
+        <Grid item xs={11} style={{alignSelf: "center"}}>
+          <Alert severity="warning">
             Make sure you have written down your Secret Recovery Phrase *offline*, you will be prompted for it next.
-          </Typography>
+          </Alert>
         </Grid>
       }
     </Grid>
