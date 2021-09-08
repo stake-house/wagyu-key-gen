ea8a53ad845fa72c66f20fa2ba5cb9d19ac3f8b8
Rémy Roy <remyroy@remyroy.com>
2021-09-08 12:35:38 -0400

    Add bundling script for Python eth2deposit_proxy and linux

--- src/scripts/bundle_proxy_linux.sh
+++ src/scripts/bundle_proxy_linux.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+PYTHONPATH=$SCRIPTPATH/../../dist/packages:$SCRIPTPATH/../vendors/eth2.0-deposit-cli-1.2.0:$(python3 -c "import sys;print(':'.join(sys.path))")
+DISTBIN=$SCRIPTPATH/../../dist/bin
+
+mkdir -p $DISTBIN
+
+PYTHONPATH=$PYTHONPATH pyinstaller --onefile --distpath $DISTBIN -p $PYTHONPATH $SCRIPTPATH/eth2deposit_proxy.py
