1f91d20c69c7369fd23e217cce26545e020d4632
Rémy Roy <remyroy@remyroy.com>
2021-11-04 11:16:26 -0400

    Add comments to BashUtils.ts

--- src/react/commands/BashUtils.ts
+++ src/react/commands/BashUtils.ts
@@ -1,8 +1,23 @@
+// BashUtils.ts
+/**
+ * This BashUtils module provides different file and OS utility functions. Those fonctions should
+ * work across all our supported operating systems including Linux, macOS and Windows.
+ * 
+ * @module
+ */
+
 import { executeCommandSync, executeCommandSyncReturnStdout } from "./ExecuteCommand";
 
 import { accessSync, constants, statSync } from "fs";
 import { fileSync } from "tmp";
 
+/**
+ * Check for the existence of a file or a directory on the filesystem.
+ * 
+ * @param filename The path to the file or directory.
+ * 
+ * @returns Returns true if the file or directory exists. Returns false if not.
+ */
 const doesFileExist = (filename: string): boolean => {
   try {
     accessSync(filename, constants.F_OK);
@@ -12,6 +27,13 @@ const doesFileExist = (filename: string): boolean => {
   }
 };
 
+/**
+ * Check for the existence of a directory on the filesystem.
+ * 
+ * @param directory The path to the directory.
+ * 
+ * @returns Returns true if the directory exists. Returns false if not.
+ */
 const doesDirectoryExist = (directory: string): boolean => {
   if (doesFileExist(directory)) {
     return statSync(directory).isDirectory();
@@ -19,11 +41,23 @@ const doesDirectoryExist = (directory: string): boolean => {
   return false;
 }
 
+/**
+ * Check if we can write a file in a directory.
+ * 
+ * @param directory The path to the directory.
+ * 
+ * @returns Returns true if the directory is writable and if a file can be written in the
+ *          directory. Returns false if not.
+ */
 const isDirectoryWritable = (directory: string): boolean => {
   let tempFile = null;
   try {
     accessSync(directory, constants.W_OK);
 
+    /**
+    * On Windows, checking for W_OK on a directory is not enough to tell if we can write a file in
+    * it. We need to actually write a temporary file to check.
+    */
     tempFile = fileSync({ keep: false, tmpdir: directory });
 
     return true;
