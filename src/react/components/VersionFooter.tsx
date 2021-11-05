12c6a2ea32da9e0315f8c3f450748a371d288369
Colfax Selby <colfax.selby@gmail.com>
2021-11-05 10:47:27 -0400

    Add VersionFooter file

---
+++ src/react/components/VersionFooter.tsx
@@ -0,0 +1,32 @@
+import { Grid, Typography } from "@material-ui/core";
+import React from "react";
+import styled from "styled-components";
+
+declare var VERSION: string;
+declare var COMMITHASH: string;
+
+const SoftText = styled(Typography)`
+  color: gray;
+  text-align: center;
+  font-size: 10px;
+`;
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 40;
+  width: 100%;
+`;
+
+const VersionFooter = () => {
+  return(
+    <Container>
+      <Grid container direction="column">
+        <Grid item justifyContent="center" xs={12}>
+          <SoftText>Version: {VERSION} - Commit Hash: {COMMITHASH}</SoftText>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default VersionFooter;
