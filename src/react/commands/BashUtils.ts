04cfb9cbb22afa8aba63d1c0c1ab65f14e9e52f6
Rémy Roy <remyroy@remyroy.com>
2021-11-06 11:57:45 -0400

    Make finished folder clickable and open keystore in file manager on click

--- src/react/commands/BashUtils.ts
+++ src/react/commands/BashUtils.ts
@@ -6,8 +6,12 @@
  * @module
  */
 
-import { accessSync, constants, statSync } from "fs";
+import { accessSync, constants, statSync, readdir, Dirent } from "fs";
 import { fileSync } from "tmp";
+import { promisify } from "util";
+import path from "path";
+
+const readdirProm = promisify(readdir);
 
 /**
  * Check for the existence of a file or a directory on the filesystem.
@@ -68,8 +72,30 @@ const isDirectoryWritable = (directory: string): boolean => {
   }
 }
 
+/**
+ * Find the first file whom filename starts with some value in a directory.
+ * 
+ * @param directory The path to the directory.
+ * @param startsWith Filename match to look for.
+ * 
+ * @returns Returns a Promise<string> that includes the path to the file if found. Returns empty
+ *          string if not found.
+ */
+const findFirstFile = async (directory: string, startsWith: string): Promise<string> => {
+  const entries = await readdirProm(directory, { withFileTypes: true });
+
+  for (const entry of entries) {
+    if (entry.isFile() && entry.name.startsWith(startsWith)) {
+      return path.join(directory, entry.name);
+    }
+  }
+
+  return "";
+}
+
 export {
   doesFileExist,
   doesDirectoryExist,
-  isDirectoryWritable
+  isDirectoryWritable,
+  findFirstFile
 };
