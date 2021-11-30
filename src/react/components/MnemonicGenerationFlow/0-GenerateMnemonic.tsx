2bd0ad9949d4e579b8e5372cf98883b3c86ebe7e
Colfax Selby <colfax.selby@gmail.com>
2021-11-30 18:18:08 -0500

    Remove stale code

--- src/react/components/MnemonicGenerationFlow/0-GenerateMnemonic.tsx
+++ src/react/components/MnemonicGenerationFlow/0-GenerateMnemonic.tsx
@@ -21,15 +21,6 @@ const LoudText = styled.span`
  * @returns 
  */
 const GenerateMnemonic: FC<GenerateMnemonicProps> = (props): ReactElement => {
-
-  const sendToKeyInfo = () => {
-    shell.openExternal("https://kb.beaconcha.in/ethereum-2-keys");
-  }
-
-  const sendToPassphraseProtection = () => {
-    shell.openExternal("https://www.ledger.com/blog/how-to-protect-your-seed-phrase");
-  }
-
   return (
     <Grid container>
       <Grid item xs={1} />
