9b808042ed7c1ad3fb92c32d34e880a8d840bc8e
Alex Peterson <alex@visnovaventures.com>
2022-02-25 17:04:11 -0600

    adopt for gnosis chain
    add gnosis chain networks

--- src/vendors/eth2.0-deposit-cli-2.0.0/staking_deposit/settings.py
+++ src/vendors/eth2.0-deposit-cli-2.0.0/staking_deposit/settings.py
@@ -13,6 +13,10 @@ MAINNET = 'mainnet'
 PRATER = 'prater'
 KINTSUGI = 'kintsugi'
 KILN = 'kiln'
+GNOSIS_TESTNET = 'gnosis-testnet'
+GNOSIS = 'gnosis'
+TEST = 'test'
+
 
 
 # Mainnet setting
@@ -23,6 +27,12 @@ PraterSetting = BaseChainSetting(NETWORK_NAME=PRATER, GENESIS_FORK_VERSION=bytes
 KintsugiSetting = BaseChainSetting(NETWORK_NAME=KINTSUGI, GENESIS_FORK_VERSION=bytes.fromhex('60000069'))
 # Merge Testnet (spec v1.1.9)
 KilnSetting = BaseChainSetting(NETWORK_NAME=KILN, GENESIS_FORK_VERSION=bytes.fromhex('70000069'))
+# Gnosis Beacon Chain testnet setting
+GnosisTestnetSetting = BaseChainSetting(NETWORK_NAME=GNOSIS_TESTNET, GENESIS_FORK_VERSION=bytes.fromhex('00006464'))
+# Gnosis Beacon Chain setting
+GnosisSetting = BaseChainSetting(NETWORK_NAME=GNOSIS, GENESIS_FORK_VERSION=bytes.fromhex('00000064'))
+# Gnosis Test
+TestSetting = BaseChainSetting(NETWORK_NAME=TEST, GENESIS_FORK_VERSION=bytes.fromhex(environ.get('GENESIS_FORK_VERSION', '12345678')))
 
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
@@ -30,6 +40,9 @@ ALL_CHAINS: Dict[str, BaseChainSetting] = {
     PRATER: PraterSetting,
     KINTSUGI: KintsugiSetting,
     KILN: KilnSetting,
+    GNOSIS_TESTNET: GnosisTestnetSetting,
+    GNOSIS: GnosisSetting,
+    TEST: TestSetting,
 }
 
 
