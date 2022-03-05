bebbd452214b87bbddc496dddeb1db14adf82e46
Alex Peterson <alex@visnovaventures.com>
2022-03-04 19:48:46 -0600

    added environ import for debug
    added environ import for debugging
    commented out the gnosis "test" settings and list.

--- src/vendors/eth2.0-deposit-cli-2.0.0/staking_deposit/settings.py
+++ src/vendors/eth2.0-deposit-cli-2.0.0/staking_deposit/settings.py
@@ -1,4 +1,5 @@
 from typing import Dict, NamedTuple
+from os import environ
 
 
 DEPOSIT_CLI_VERSION = '2.0.0'
@@ -10,17 +11,19 @@ class BaseChainSetting(NamedTuple):
 
 
 MAINNET = 'mainnet'
+GNOSIS = 'gnosis'
 PRATER = 'prater'
 KINTSUGI = 'kintsugi'
 KILN = 'kiln'
 GNOSIS_TESTNET = 'gnosis-testnet'
-GNOSIS = 'gnosis'
-TEST = 'test'
+#TEST = 'test'
 
 
 
 # Mainnet setting
 MainnetSetting = BaseChainSetting(NETWORK_NAME=MAINNET, GENESIS_FORK_VERSION=bytes.fromhex('00000000'))
+# Gnosis Mainnet Beacon Chain setting
+GnosisSetting = BaseChainSetting(NETWORK_NAME=GNOSIS, GENESIS_FORK_VERSION=bytes.fromhex('00000064'))
 # Testnet (spec v1.0.1)
 PraterSetting = BaseChainSetting(NETWORK_NAME=PRATER, GENESIS_FORK_VERSION=bytes.fromhex('00001020'))
 # Merge Testnet (spec v1.1.4)
@@ -29,10 +32,8 @@ KintsugiSetting = BaseChainSetting(NETWORK_NAME=KINTSUGI, GENESIS_FORK_VERSION=b
 KilnSetting = BaseChainSetting(NETWORK_NAME=KILN, GENESIS_FORK_VERSION=bytes.fromhex('70000069'))
 # Gnosis Beacon Chain testnet setting
 GnosisTestnetSetting = BaseChainSetting(NETWORK_NAME=GNOSIS_TESTNET, GENESIS_FORK_VERSION=bytes.fromhex('00006464'))
-# Gnosis Beacon Chain setting
-GnosisSetting = BaseChainSetting(NETWORK_NAME=GNOSIS, GENESIS_FORK_VERSION=bytes.fromhex('00000064'))
 # Gnosis Test
-TestSetting = BaseChainSetting(NETWORK_NAME=TEST, GENESIS_FORK_VERSION=bytes.fromhex(environ.get('GENESIS_FORK_VERSION', '12345678')))
+#TestSetting = BaseChainSetting(NETWORK_NAME=TEST, GENESIS_FORK_VERSION=bytes.fromhex(environ.get('GENESIS_FORK_VERSION', '12345678')))
 
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
@@ -42,7 +43,7 @@ ALL_CHAINS: Dict[str, BaseChainSetting] = {
     KILN: KilnSetting,
     GNOSIS_TESTNET: GnosisTestnetSetting,
     GNOSIS: GnosisSetting,
-    TEST: TestSetting,
+    #TEST: TestSetting,
 }
 
 
