540e8a653e63e7bd32a8dd5e489d096cb0471ec6
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-02-02 19:36:31 -0800

    Revert "make providing withdrawal creds required"
    This reverts commit b84f00e494c17652525a4bebbaa4ed3b6aafeef0.

--- src/react/constants.ts
+++ src/react/constants.ts
@@ -20,7 +20,7 @@ export const tooltips = {
 	NUMBER_OF_KEYS: "Enter how many new validator keys you'd like to create.",
 	PASSWORD: "Pick a strong password (at least 8 characters) that will be used to protect your keys.",
 	STARTING_INDEX: "Each key is created sequentially, so we need to know how many you've created with this Secret Recovery Phrase in the past in order to create some new ones for you.",
-	ETH1_WITHDRAW_ADDRESS: "An Ethereum address for the withdrawal credentials.",
+	ETH1_WITHDRAW_ADDRESS: "An optional Ethereum address for the withdrawal credentials.",
 };
 
 export const stepLabels = {
