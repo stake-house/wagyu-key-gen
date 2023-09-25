b924b12782229a369674fe038cc07571c0a4e80c
Rémy Roy <remyroy@remyroy.com>
2023-09-25 12:39:03 -0400

    Update for sdc 2.7.0

--- src/scripts/bundle_proxy_linux.sh
+++ src/scripts/bundle_proxy_linux.sh
@@ -10,7 +10,7 @@ fi
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-EDCDIR=staking-deposit-cli-2.6.0
+EDCDIR=staking-deposit-cli-2.7.0
 
 TARGETPACKAGESPATH=$SCRIPTPATH/../../dist/packages
 ETH2DEPOSITCLIPATH=$SCRIPTPATH/../vendors/$EDCDIR
